99a27a9522ba40a30f589aa10ccc613835cc092c
Michael <114503192+MichaelC3@users.noreply.github.com>
2023-07-18 10:54:01 -0500

    Uploaded modules script

---
+++ Delta/modules.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+module load cue-login-env/1.0
+module load default
+module load modtree/cpu
+module load intel-oneapi-mkl/2022.0.2
+module load gcc/11.2.0
+module load openblas/0.3.20
+module load rdma-core/32.0
+module load ucx/1.11.2
+module load knem/1.1.4
+module load libevent/2.1.8
+module load libfabric/1.14.0
+module load lustre/2.14.0_ddn23
+module load openssh/8.0p1
+module load pmix/3.2.3
+module load openmpi/4.1.2
+module load hdf5/1.13.1
+module load anaconda3_cpu/4.13.0
+module load gsl/2.7
